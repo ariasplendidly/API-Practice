699dafb75f9ddc70b982d2e7b4b5f00820045eb4
ariasplendidly <102254554+ariasplendidly@users.noreply.github.com>
2023-04-16 18:32:26 -0700

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'api-practice';
+  users: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.http.get('https://reqres.in/api/users')
+      .subscribe((data: any) => {
+        this.users = data.data;
+      });
+  }
 }
